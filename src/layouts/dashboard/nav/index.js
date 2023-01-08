a422edefac742901a54025c5348de947fec3c244
kkkwp <chxrryda@gmail.com>
2023-01-08 22:37:15 +0900

    feat: drawer 수정

--- src/layouts/dashboard/nav/index.js
+++ src/layouts/dashboard/nav/index.js
@@ -3,7 +3,7 @@ import { useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 // @mui
 import { styled, alpha } from '@mui/material/styles';
-import { Box, Link, Drawer, Typography, Avatar } from '@mui/material';
+import { Box, Link, Drawer, Typography, Avatar, Stack, Button } from '@mui/material';
 // mock
 import account from '../../../_mock/account';
 // hooks
@@ -75,7 +75,30 @@ export default function Nav({ openNav, onCloseNav }) {
         </Link>
       </Box>
 
+      {/* following & follower */}
+      <Box sx={{ display: 'flex', mb: 2.5, mx: 2.5 }}>
+
+        <Typography variant="h6" sx={{mr: 2}}>
+          follower: {}
+        </Typography>
+
+        <Typography variant="h6">
+          following: {}
+        </Typography>
+      </Box>
+
       <NavSection data={navConfig} />
+
+      <Box sx={{ flexGrow: 1 }} />
+
+      {/* logout */}
+      <Box sx={{ px: 2.5, pb: 3, mt: 10 }}>
+        <Stack alignItems="center" spacing={3} sx={{ pt: 5, borderRadius: 2, position: 'relative' }}>
+          <Button href="/login" sx={{ width: 1 }}>
+            Logout
+          </Button>
+        </Stack>
+      </Box>
 
     </Scrollbar>
   );
