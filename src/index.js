84b6724cd9dcca2f88cddf3d8ac4890732c16979
kkkwp <chxrryda@gmail.com>
2023-01-30 09:41:57 +0900

    refactor: index의 안 쓰는 기능 제외

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
-
-import * as serviceWorker from './serviceWorker';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,11 +12,3 @@ root.render(
     </BrowserRouter>
   </HelmetProvider>
 );
-
-// If you want to enable client cache, register instead.
-serviceWorker.unregister();
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
